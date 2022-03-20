e5ca448bce647078a6936be60be26f5f2611153f
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-20 21:22:36 +0300

    bug:this will fix a bug in the orderform.js

--- js/orderform.js
+++ js/orderform.js
@@ -1,8 +1,9 @@
-let pizzaOrder = new Pizza();
-pizzaOrder.toppings = [];
+
 
 const listenForTypeChange = (e) => {
-    pizzaOrder.name = e.value;
+    if (e.value !== '') {
+        pizzaOrder.name = e.value;
+    }
 }
 
 const listenForToppingsChange = (e) => {
@@ -18,14 +19,18 @@ const listenForToppingsChange = (e) => {
 }
 
 const listenForSizeChange = (e) => {
-    const size_ = pizzaSizes.find(size => size.name === e.value);
-    pizzaOrder.size = size_.name;
-    pizzaOrder.price = size_.price;
+    if (!e.value !== '') {
+        const size_ = pizzaSizes.find(size => size.name === e.value);
+        pizzaOrder.size = size_.name;
+        pizzaOrder.price = size_.price;
+    }
 }
 
 const listenForCrustChange = (e) => {
-    const crust_ = pizzaCrusts.find(crust => crust.name === e.value);
-    pizzaOrder.crust = crust_;
+    if (!e.value !== '') {
+        const crust_ = pizzaCrusts.find(crust => crust.name === e.value);
+        pizzaOrder.crust = crust_;
+    }
 }
 
 const listenForQuantityChange = (e) => {
@@ -34,27 +39,87 @@ const listenForQuantityChange = (e) => {
 
 // const
 
-order_form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (pizzaOrder.name === undefined) {
-        alert('Please select a pizza type/flavour');
-        return;
-    }
+if (order_form) {
+    order_form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (pizzaOrder.name === undefined || pizzaOrder.name === '') {
+            alert('Please select a pizza type/flavour');
+            return;
+        }
 
-    if (pizzaOrder.toppings.length === 0) {
-        alert('Please select at least one topping');
-        return;
-    }
+        if (pizzaOrder.toppings.length === 0) {
+            alert('Please select at least one topping');
+            return;
+        }
 
-    if (pizzaOrder.size === undefined || pizzaOrder.price === undefined) {
-        alert('Please select pizza size');
-        return;
-    }
+        if (pizzaOrder.size === undefined || pizzaOrder.price === undefined) {
+            alert('Please select pizza size');
+            return;
+        }
 
-    if (pizzaOrder.crust === undefined) {
-        alert('Please select a crust');
-        return;
-    }
+        if (pizzaOrder.crust === undefined) {
+            alert('Please select a crust');
+            return;
+        }
 
-    alert(pizzaOrder.calculatePrice());
-});
+        makeOrder(pizzaOrder, pizza_quantity.value);
+        // console.log('cart total', cart.getTotalPrice())
+    });
+}
+
+const makeOrder = (pizza_order, quantity) => {
+    const singlepizzaInCart = new SinglePizzaInCart(pizza_order, quantity);
+    cart.addPizza(singlepizzaInCart);
+    alert("Your pizza has been added to the cart, proceed to checkout");
+    window.location.href = './pages/checkout.html';
+}
+
+if (checkout_form) {
+    checkout_form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (name_.value === '') {
+            alert('Please enter your name');
+            return;
+        }
+
+        if (phone.value === '') {
+            alert('Please enter your phone number');
+            return;
+        }
+
+        if (address.value === '') {
+            alert('Please enter your address');
+            return;
+        }
+
+        if (cart.getCart().pizzasincart.length === 0) {
+            alert('Please add atleast a pizza to the cart before proceeding to delivery');
+            return;
+        }
+
+        placeOrder();
+
+    });
+}
+
+function placeOrder() {
+    order.addOrder(cart.getCart());
+    alert('Your order has been placed, thank you for shopping with us.');
+    cart.clearCart();
+    window.location.href = './orders.html';
+}
+
+function clearCart() {
+    cart.clearCart();
+    window.location.reload();
+}
+
+const addToCart = (pizza_name) => {
+
+    const pizza_ = pizzas.find(pizza => pizza.name === pizza_name);
+    const singlepizzaInCart = new SinglePizzaInCart(pizza_, 1);
+    cart.addPizza(singlepizzaInCart);
+
+    alert('The pizza has been added to the cart, please proceed to checkout');
+
+}

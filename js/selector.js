edbcfe3f284bf206163792d842133f9ee7c332be
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-20 07:35:10 +0300

    feature:This will render new file with selectors from html

---
+++ js/selector.js
@@ -0,0 +1,9 @@
+let pizzasHolder = document.getElementById('pizzas-holder');
+
+let order_form = document.getElementById('order-form');
+
+let pizza_type = document.getElementById('pizza-type');
+let pizza_crust = document.getElementById('pizza-crust');
+let pizza_size = document.getElementById('pizza-size');
+let pizza_toppings = document.getElementById('pizza-toppings');
+let pizza_quantity = document.getElementById('pizza-quantity');

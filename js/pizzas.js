f65b6fc64162303926c6620f9fd748e81a77320e
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-20 07:33:12 +0300

    functionality:This will render the flavoured pizzas and their price calculation

--- js/pizzas.js
+++ js/pizzas.js
@@ -1,10 +1,10 @@
 class Pizza {
-    constructor(name, crust, toppings, pizzasize, image) {
+    constructor(name, crust, toppings, pizzasize, image = 'https://www.picng.com/upload/pizza/png_pizza_15268.png') {
         this.name = name;
         this.toppings = toppings;
         this.crust = crust;
-        this.price = pizzasize.price;
-        this.size = pizzasize.name;
+        this.price = pizzasize?.price;
+        this.size = pizzasize?.name;
         this.image = image;
     }
 
@@ -49,8 +49,20 @@ class PizzaSize {
     }
 }
 
+class SinglePizzaInCart {
+    constructor(id, pizza, quantity = 1) {
+        this.id = id;
+        this.pizza = pizza;
+        this.quantity = quantity;
+    }
 
-const toppings = [
+    getTotalPrice() {
+        return this.pizza.calculatePrice() * this.quantity;
+    }
+}
+
+
+const pizzaToppings = [
     new Topping('Tomato', 80),
     new Topping('Mushroom', 80),
     new Topping('Veggie Salad', 80),
@@ -63,7 +75,7 @@ const toppings = [
     new Topping('peperroni Chicken', 180),
 ]
 
-const crusts = [
+const pizzaCrusts = [
     new Crust('Thin crust', 150),
     new Crust('Thick crust', 250),
     new Crust('Flat Bread crust', 280),
@@ -78,18 +90,15 @@ const pizzaSizes = [
 ]
 
 const pizzas = [
-    new Pizza('Meat Deluxe', crusts[0], toppings.slice(0, 3), pizzaSizes[0], 'https://www.pngkit.com/png/full/0-5891_large-pizza-slice-transparent-background-pizza-slice-png.png'),
-    new Pizza('Chicken Hawaiian', crusts[1], toppings.slice(3, 6), pizzaSizes[1], 'https://www.downloadclipart.net/medium/pizza-slice-transparent-background.png'),
-    new Pizza('BBO Steak', crusts[2], toppings.slice(6, 9), pizzaSizes[2], 'https://www.picng.com/upload/pizza/png_pizza_15268.png'),
-    new Pizza('Chicken Macon BBQ', crusts[1], toppings.slice(7, 9), pizzaSizes[3], 'https://www.kindpng.com/picc/m/106-1065850_pizza-hut-poster-dough-transparent-background-pizza-hut.png'),
-    new Pizza('Chicken & Mushroom', crusts[0], toppings.slice(4, 7), pizzaSizes[2], 'https://www.nicepng.com/png/full/830-8300698_pizza-dominos-pizza-transparent-background.png'),
-    new Pizza('Spicy Boerewors', crusts[2], toppings.slice(4, 9), pizzaSizes[1], 'https://www.freeiconspng.com/uploads/pizza-png-33.png'),
-    new Pizza('Beef pepporoni', crusts[1], toppings.slice(2, 5), pizzaSizes[2], 'https://www.picng.com/upload/pizza/png_pizza_15278.png'),
+    new Pizza('Meat Deluxe', pizzaCrusts[0], pizzaToppings.slice(0, 3), pizzaSizes[0], 'https://www.pngkit.com/png/full/0-5891_large-pizza-slice-transparent-background-pizza-slice-png.png'),
+    new Pizza('Chicken Hawaiian', pizzaCrusts[1], pizzaToppings.slice(3, 6), pizzaSizes[1], 'https://www.downloadclipart.net/medium/pizza-slice-transparent-background.png'),
+    new Pizza('BBO Steak', pizzaCrusts[2], pizzaToppings.slice(6, 9), pizzaSizes[2], 'https://www.picng.com/upload/pizza/png_pizza_15268.png'),
+    new Pizza('Chicken Macon BBQ', pizzaCrusts[1], pizzaToppings.slice(7, 9), pizzaSizes[3], 'https://www.kindpng.com/picc/m/106-1065850_pizza-hut-poster-dough-transparent-background-pizza-hut.png'),
+    new Pizza('Chicken & Mushroom', pizzaCrusts[0], pizzaToppings.slice(4, 7), pizzaSizes[2], 'https://www.nicepng.com/png/full/830-8300698_pizza-dominos-pizza-transparent-background.png'),
+    new Pizza('Spicy Boerewors', pizzaCrusts[2], pizzaToppings.slice(4, 9), pizzaSizes[1], 'https://www.freeiconspng.com/uploads/pizza-png-33.png'),
+    new Pizza('Beef pepporoni', pizzaCrusts[1], pizzaToppings.slice(2, 5), pizzaSizes[2], 'https://www.picng.com/upload/pizza/png_pizza_15278.png'),
 ]
 
-pizzas.forEach(function (pizza) {
-    console.log(`${pizza.name} - ${pizza.size} - ${pizza.crust.name} - ${pizza.calculatePrice()}`);
-}
-);
+
 
 
